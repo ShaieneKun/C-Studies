f710b60a7792088edf7cc790664a3d7eefd4423d
ShaieneKun <rafaelshaiene115@gmail.com>
2024-05-29 04:04:27 +0000

    2nd commit

---
+++ course-1/helloWorld/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 10, length = 10;
+
+Console.WriteLine(a);
+
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine("Iteration: " + i);
+}
